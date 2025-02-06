a9b25a2687a174097e70de22ccd2712a940c2b28
rustynbustedup <2rustynbustedup@gmail.com>
2025-02-06 10:17:29 -0500

    started unit 2 project

---
+++ Data.py
@@ -0,0 +1,30 @@
+#hello
+
+""" x=3
+y = float(3)
+print(x,y) """
+
+
+""" values = [1,2.23,5,7,2,30,15]
+print(values)
+for i in values:
+    print(values[0])
+    print(values[6]) """
+
+""" x = "this is a thing"
+y= x.split( )
+z = y[0]
+print(y)
+print(z) """
+
+def sentence():
+    answer = input ("pease emter semtanse")
+    def count_words(sentence)
+    words = answer.split()
+    hello = len(words)
+    int(hello)
+    for words in words:
+        print(word)
+sentance()
+
+
