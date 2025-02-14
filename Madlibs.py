542d11e83947acfa027f4f19555b1bd9911e69a0
rustynbustedup <2rustynbustedup@gmail.com>
2025-02-14 11:08:52 -0500

    Added words to madlibs proj

---
+++ Madlibs.py
@@ -0,0 +1,5 @@
+#hello this is the start of the madlibs project 
+verbs = [racing,flying,dying,sleeping,Defenestrate]
+nouns = [Nudiustertian,Agelast,Galoot,Fainéant]
+numbers = [4,6.2,28,9631,91,19]
+celebrity = [Tom Holland,Baby Gronk,Johnny Depp,Tom Cruise]
